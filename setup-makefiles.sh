cd7eee9be585ce18b17191e00be8e26bd3d0fdbd
keneankit01 <keneankit01@gmail.com>
2021-07-24 22:07:06 +0530

    sdm660-common: ADD B2N To Build Guards
    Change-Id: I8b95d7f74c64d8fdd2b333d057493e098c16b3e4
    Signed-off-by: Timotej Šulík <timotej.sulik@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -25,7 +25,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
-write_headers "DDV DRG PL2"
+write_headers "B2N DDV DRG PL2"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
